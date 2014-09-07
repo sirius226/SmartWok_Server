d5383a77a47575512a6caf4357a1c235bfdcdf30
sirius226 <394544256@qq.com>
2014-09-07 01:53:00 -0400

    first commit, user login module, function: authentication, registration, email comfirmation, account management

---
+++ config.py
@@ -0,0 +1,45 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'smartwokmailserver' or os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = 'lc5115800' or os.environ.get('lc5115800')
+    SMARTWOK_MAIL_SUBJECT_PREFIX = '[SmartWok]'
+    SMARTWOK_MAIL_SENDER = 'SmartWok Admin <smartwokmailserver@gmail.com>'
+    SMARTWOK_ADMIN = os.environ.get('SMARTWOK_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
